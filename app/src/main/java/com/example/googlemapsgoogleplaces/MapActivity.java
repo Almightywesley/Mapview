e7a978c0b5a78f819fe6d88a9f365a591db0c268
Wesley <keesdelol@hotmail.com>
2019-03-12 10:01:05 +0100

    2.0 :)

--- app/src/main/java/com/example/googlemapsgoogleplaces/MapActivity.java
+++ app/src/main/java/com/example/googlemapsgoogleplaces/MapActivity.java
@@ -2,6 +2,9 @@ package com.example.googlemapsgoogleplaces;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,19 +12,49 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "msp is ready", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "map is ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            init();
+        }
 
     }
 
@@ -30,16 +63,97 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    // widgets
+    private EditText mSearchText
+
 
     //vars
     private Boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mSearchText = (EditText) findViewById(R.id.input_search);
         getLocationPermission();
+    }
+
+    private void init(){
+        Log.d(TAG,"init: initializing");
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                || actionId == EditorInfo.IME_ACTION_DONE
+                || event.getAction() == KeyEvent.ACTION_DOWN
+                || event.getAction() == KeyEvent.KEYCODE_ENTER){
+                    // execute our methode for seaching
+                    geoLocate();
+
+                }
+                return false;
+            }
+        });
+    }
+    private void geoLocate(){
+        Log.d(TAG, "geolocating"){
+            String searchingString = mSearchText.getText().toString();
+            Geocoder geocoder = new Geocoder(MapActivity.this);
+            List<Address> list = new ArrayList<>();
+            try{
+                list = geocoder.getFromLocationName(searchingString,1);
+
+
+            }catch (IOException e){
+                Log.e(TAG, "geoLocate: IOException" + e.getMessage());
+            }
+            if (list.size() > 0){
+                Address address = list.get(0);
+                Log.d(TAG,"geoLocate: found a location" + address.toString());
+                //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void getDeviceLocation(){
+        Log.d(TAG,"getDeviceLocation: get device current location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if(mLocationPermissionGranted){
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                       if (task.isSuccessful()){
+                           Log.d(TAG,"onComplete: found location");
+                           Location currentLocation = (Location) task.getResult();
+
+                           moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                   DEFAULT_ZOOM);
+
+                       } else {
+                           Log.d(TAG,"onComplete: current location is null");
+                           Toast.makeText(MapActivity.this,"unable to get current location",Toast.LENGTH_SHORT).show();
+                       }
+                    }
+                });
+            }
+        } catch (SecurityException e){
+            Log.e(TAG,"getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG,"moveCamera: moving camera to lst:" + latLng.latitude + ", lng: "+ latLng.longitude );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+
     }
 
     private void initMap() {
@@ -57,13 +171,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                Boolean mLocationPermissionGranted =true;
+                mLocationPermissionGranted = true;
+                initMap();
             } else {
                 ActivityCompat.requestPermissions(this,
                         permission,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
 
+        }else {
+            ActivityCompat.requestPermissions(this,
+                    permission,
+                    LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
