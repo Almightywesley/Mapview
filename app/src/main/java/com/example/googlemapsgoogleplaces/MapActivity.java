e56711bf5fcbaec4bb7f1e7e5f056966bbaf5ed9
Wesley <keesdelol@hotmail.com>
2019-03-19 10:20:16 +0100

    4.0 :)

--- app/src/main/java/com/example/googlemapsgoogleplaces/MapActivity.java
+++ app/src/main/java/com/example/googlemapsgoogleplaces/MapActivity.java
@@ -13,8 +13,11 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +29,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
@@ -67,6 +71,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     // widgets
     private EditText mSearchText;
+    private ImageView mGps;
 
 
     //vars
@@ -81,6 +86,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         mSearchText = (EditText) findViewById(R.id.input_search);
+        mGps = (ImageView) findViewById(R.id.ic_gps);
         getLocationPermission();
     }
 
@@ -101,6 +107,17 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 return false;
             }
         });
+
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"Onclick: clicked gps icon");
+                getDeviceLocation();
+
+            }
+        });
+        hideSoftKeyboard();
+
     }
     private void geoLocate(){
         Log.d(TAG, "geolocating");
@@ -118,6 +135,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Address address = list.get(0);
                 Log.d(TAG,"geoLocate: found a location" + address.toString());
                 //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+                moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),DEFAULT_ZOOM,
+                        address.getAddressLine(0));
             }
         }
 
@@ -136,7 +155,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                            Location currentLocation = (Location) task.getResult();
 
                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
-                                   DEFAULT_ZOOM);
+                                   DEFAULT_ZOOM, "My location");
 
                        } else {
                            Log.d(TAG,"onComplete: current location is null");
@@ -150,10 +169,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
+    private void moveCamera(LatLng latLng, float zoom, String title){
         Log.d(TAG,"moveCamera: moving camera to lst:" + latLng.latitude + ", lng: "+ latLng.longitude );
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
 
+        if (!title.equals("my location")){
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title(title);
+        mMap.addMarker(options);
+
+        }
+        hideSoftKeyboard();
     }
 
     private void initMap() {
@@ -211,6 +238,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 }
             }
         }
+    }
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
 
